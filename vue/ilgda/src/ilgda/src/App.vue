b9bb7c87dba44c55ceb2a5d78f475c0d5fa2062d
gongahkia <angryapplegravy@gmail.com>
2025-02-24 16:04:03 +0800

    feat(app): stop this shit

---
+++ vue/ilgda/src/ilgda/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1></h1>
+    <BookSearch />
+  </div>
+</template>
+
+<script>
+import BookSearch from './components/BookSearch.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BookSearch
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
