bc39dcc9c162b0965a496e7a3f03759cfa930c0e
gongahkia <angryapplegravy@gmail.com>
2025-02-28 16:40:49 +0800

    feat(app): added

---
+++ javascript/learning_solidjs/learning-solidjs-music-app/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Bandsintown App</h1>
+      <p>Search for your favorite artists and their upcoming events.</p>
+    </div>
+  );
+}
+
+export default Home;
