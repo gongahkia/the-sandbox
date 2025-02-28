ae5f0c9bf0d85659301bf728c9cd2f863549d648
gongahkia <angryapplegravy@gmail.com>
2025-02-28 16:20:13 +0800

    feat(app): learning angular

---
+++ angular/angular-stock-app/angular-stock-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { OptionCalculatorComponent } from './option-calculator/option-calculator.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OptionCalculatorComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
